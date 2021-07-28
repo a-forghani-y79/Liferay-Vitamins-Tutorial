0213fb00de2e019fc9ef291bf2edd7cacea76ac7
ali forghani <a.forghani.y79@gmail.com>
2021-07-28 15:32:57 +0430

    restBuild

---
+++ modules/headless-vitamins/headless-vitamins-impl/src/main/java/com/denbinger/vitamins/internal/graphql/servlet/v1_0/ServletDataImpl.java
@@ -0,0 +1,41 @@
+package com.denbinger.vitamins.internal.graphql.servlet.v1_0;
+
+import com.denbinger.vitamins.internal.graphql.mutation.v1_0.Mutation;
+import com.denbinger.vitamins.internal.graphql.query.v1_0.Query;
+
+import com.liferay.portal.vulcan.graphql.servlet.ServletData;
+
+import javax.annotation.Generated;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author jinos
+ * @generated
+ */
+@Component(immediate = true, service = ServletData.class)
+@Generated("")
+public class ServletDataImpl implements ServletData {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+	}
+
+	@Override
+	public Mutation getMutation() {
+		return new Mutation();
+	}
+
+	@Override
+	public String getPath() {
+		return "/headless-vitamins-graphql/v1_0";
+	}
+
+	@Override
+	public Query getQuery() {
+		return new Query();
+	}
+
+}
---
+++ modules/headless-vitamins/headless-vitamins-impl/src/main/java/com/denbinger/vitamins/internal/jaxrs/application/HeadlessVitaminsApplication.java
@@ -0,0 +1,24 @@
+package com.denbinger.vitamins.internal.jaxrs.application;
+
+import javax.annotation.Generated;
+
+import javax.ws.rs.core.Application;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author jinos
+ * @generated
+ */
+@Component(
+	property = {
+		"liferay.jackson=false",
+		"osgi.jaxrs.application.base=/headless-vitamins",
+		"osgi.jaxrs.extension.select=(osgi.jaxrs.name=Liferay.Vulcan)",
+		"osgi.jaxrs.name=HeadlessVitamins"
+	},
+	service = Application.class
+)
+@Generated("")
+public class HeadlessVitaminsApplication extends Application {
+}
